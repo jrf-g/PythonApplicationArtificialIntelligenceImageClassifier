335715b5c9336e35b23d8437489519510e81a10e
Jackson F <jrfaragalla@gmail.com>
2026-05-16 10:41:14 -0700

    coffee fixed

--- congrats.coffee
+++ congrats.coffee
@@ -4,8 +4,6 @@ rl = readline.createInterface
   input: process.stdin
   output: process.stdout
 
-
-enter -> console.log("please run directly")
 rl.question "Enter your name: ", (answer) ->
 console.log "Thank you for Testing my AI, #{answer}! Please give feedback on the github page specified in package.json"
 rl.close()
--- executer.sh
+++ executer.sh
@@ -1,5 +1,32 @@
+echo "STOP THIS SCRIPT IF YOU ARENT RUNNING FROM THE CLONED REPO"
+echo "YOU HAVE TEN SECONDS TO STOP THE SCRIPT"
+sleep 10
+echo "TRAINING"
 python optimizedcputrainer.py
+echo "WAITING"
+sleep 1
+echo "PREDICTING"
 python classifyandpredict.py
+echo "FEEDBACK FORM AND PARTY PROGRAM LAUNCHING"
+sleep 1
 coffee -c congrats.coffee -o javascript/ -r readline --no-header
 cd javascript
 node congrats.js
+echo "please wait. cleanup script."
+cd -
+echo "DONE"
+read -p "DELETE TRAINING DATA? (requires rebuild of classify.png and samples. we only rebuild directories but not their contents) (y/n): " choice
+if [[ "$choice" != "y" ]]; then
+    echo "aborting in 10"
+	sleep 10
+    exit
+fi
+rm classify.png
+rm -r samples
+rm -r javascript
+mkdir samples
+mkdir javascript
+echo "cleanup complete. please rebuild"
+echo "aborting in 10"
+sleep 10
+exit
